7f70fbd49ceb2c6f318d9e9b9b9c1b64859d8392
Marco Morales <mmorales@gmx.ch>
2019-11-19 19:44:35 +0100

    updates

--- db_eda/utils.py
+++ db_eda/utils.py
@@ -1,4 +1,8 @@
 import sqlite3
+import os
+import json
+from pandas.io.json import json_normalize
+import pandas as pd
 
 
 def create_connection(db_file):
@@ -14,3 +18,22 @@ def create_connection(db_file):
         print(e)
  
     return conn
+
+
+def get_topic_df(path, modus):
+    """"""
+    # list of files
+    onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+    
+    if modus=='SUBSET':
+        onlyfiles = onlyfiles[0:100]
+            
+    data = pd.DataFrame()
+    for file in onlyfiles:
+        with open(os.path.join(path, file)) as json_file:
+            datatemp = json.load(json_file)
+        datanorm = json_normalize(datatemp['response']['topics'])
+        datanorm['file_name'] = file
+        datanorm['PK_ID_CLEAN'] = file.split('_')[0]
+        data = data.append(datanorm)
+    return data
